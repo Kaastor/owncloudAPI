a5bcff341dae8cb3490897d521350a1c795dabd0
Kaastor <czuba.przemyslaw@gmail.com>
2016-07-19 09:27:32 +0200

    OwncloudUploadService class body added

--- src/main/java/OwncloudUploadService.java
+++ src/main/java/OwncloudUploadService.java
@@ -1,6 +1,33 @@
+import com.anrisoftware.simplerest.core.SimpleRestException;
+import com.anrisoftware.simplerest.owncloud.DefaultOwncloudAccount;
+import com.anrisoftware.simplerest.owncloud.DefaultOwncloudAccount.DefaultOwncloudAccountFactory;
+import com.anrisoftware.simplerest.owncloud.OwncloudUploadFile;
+import com.anrisoftware.simplerest.owncloudocs.OwncloudOcsUploadFile.OwncloudOcsUploadFileFactory;
+import com.google.inject.Inject;
+import org.apache.http.entity.ContentType;
+
+import java.io.File;
+import java.net.URI;
+
 /**
  * Created by Przemek on 2016-07-19.
  */
 public class OwncloudUploadService {
 
+    DefaultOwncloudAccountFactory accountFactory;
+    OwncloudOcsUploadFileFactory uploadFileFactory;
+
+    @Inject
+    public OwncloudUploadService(DefaultOwncloudAccountFactory accountFactory, OwncloudOcsUploadFileFactory uploadFileFactory){
+        this.accountFactory =  accountFactory;
+        this.uploadFileFactory =  uploadFileFactory;
+    }
+
+    public void requestStatus(URI uri) throws SimpleRestException {
+        DefaultOwncloudAccount account = accountFactory.create("przemys", "przemys", uri);
+        OwncloudUploadFile upload = uploadFileFactory.create(account, new File("C:\\Users\\Przemek\\Desktop\\Czystykod.pdf"),
+                "Czystykod.pdf", ContentType.create("text/plain", "UTF-8"));
+        upload.call();
+
+    }
 }
