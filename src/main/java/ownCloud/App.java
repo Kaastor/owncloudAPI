cd4e8a6ce6cbd1ef12a284843f1b525238be3e4f
Kaastor <czuba.przemyslaw@gmail.com>
2016-07-20 18:33:41 +0200

    adding OwncloudServices

--- src/main/java/ownCloud/App.java
+++ src/main/java/ownCloud/App.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 import java.io.File;
-import java.net.URL;
+
 
 /**
  * Created by Przemek on 2016-07-19.
@@ -18,22 +18,11 @@ import java.net.URL;
 @ComponentScan("ownCloud")
 class App {
 
-    @SneakyThrows
     public static void main(String[] args){
         ApplicationContext ctx = SpringApplication.run(App.class, args);
-        OwncloudUploadService owncloudUploadService = (OwncloudUploadService) ctx.getBean("owncloudUploadService");
-        owncloudUploadService.uploadFile("przemys", "przemys",
-                new URL("http://192.168.179.131/owncloud").toURI(),
-                new File("C:\\Users\\Przemek\\Desktop\\sunnyforest.jpg"), "Photos/sunny-forest.jpg");
-
-
-        OwncloudDownloadService owncloudDownloadService = (OwncloudDownloadService) ctx.getBean("owncloudDownloadService");
-        owncloudDownloadService.downloadFile("przemys", "przemys",
-                new URL("http://192.168.179.131/owncloud").toURI(),
-                "Documents/Czystykod.pdf", "C:\\Users\\Przemek\\Desktop\\CzystyKod.pdf");
-
-
-
+        OwncloudServices owncloudServices = (OwncloudServices) ctx.getBean("owncloudServices");
+        owncloudServices.uploadFile( new File("C:\\Users\\Przemek\\Desktop\\sunnyforest.jpg"), "Photos/sunny-forest.jpg");
+        owncloudServices.downloadFile("Documents/Czystykod.pdf", "C:\\Users\\Przemek\\Desktop\\CzystyKod.pdf");
     }
 
 
--- src/main/java/ownCloud/OwncloudServices.java
+++ src/main/java/ownCloud/OwncloudServices.java
@@ -2,15 +2,19 @@ package ownCloud;
 
 import com.anrisoftware.simplerest.owncloud.DefaultOwncloudAccount;
 import com.anrisoftware.simplerest.owncloud.DefaultOwncloudAccount.DefaultOwncloudAccountFactory;
+import com.anrisoftware.simplerest.owncloud.OwncloudUploadFile;
+import com.anrisoftware.simplerest.owncloudocs.OwncloudOcsUploadFile.OwncloudOcsUploadFileFactory;
 import lombok.SneakyThrows;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -18,24 +22,33 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
- * Created by Przemek on 2016-07-20.
+ * Created by Przemek on 2016-07-19.
  *
  */
 @Component
-class OwncloudDownloadService {
+class OwncloudServices {
 
     private DefaultOwncloudAccountFactory accountFactory;
+    private OwncloudOcsUploadFileFactory uploadFileFactory;
     private DefaultOwncloudAccount account;
 
     @Autowired
-    public void setOwncloudUploadService(DefaultOwncloudAccountFactory accountFactory){
+    @SneakyThrows
+    public void setOwncloudUploadService(DefaultOwncloudAccountFactory accountFactory, OwncloudOcsUploadFileFactory uploadFileFactory){
         this.accountFactory =  accountFactory;
+        this.uploadFileFactory =  uploadFileFactory;
+        this.account = accountFactory.create("przemys", "przemys", new URI("http://192.168.179.131/owncloud"));
     }
 
     @SneakyThrows
-    public void downloadFile(String user, String password, URI baseUri, String remotePath, String fileDestinationPath) {
+    public void uploadFile(File file, String remotePath){
+        OwncloudUploadFile upload = uploadFileFactory.create(account, file,
+                remotePath, ContentType.create("text/plain", "UTF-8"));
+        upload.call();
+    }
 
-        account = accountFactory.create(user, password, baseUri);
+    @SneakyThrows
+    public void downloadFile(String remotePath, String fileDestinationPath) {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpget = new HttpGet(getRequestURI(remotePath));
         HttpResponse response = httpClient.execute(httpget);
